80a1c476bb2109c8fd7f4191a4bc97b98fb68468
Wylderfan <pulseconnor@gmail.com>
2025-07-08 09:38:13 -0700

    Integrated Harpoon for file navigation and updated key mappings

--- lua/wylderfan/packer.lua
+++ lua/wylderfan/packer.lua
@@ -3,10 +3,18 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  
+
+  -- Harpoon
+  use "nvim-lua/plenary.nvim"
+  use {
+      "ThePrimeagen/harpoon",
+      branch = "harpoon2",
+      requires = { {"nvim-lua/plenary.nvim"} }
+  }
+
+  -- Telescope
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	-- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
@@ -19,6 +27,7 @@ return require('packer').startup(function(use)
 })
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('tpope/vim-fugitive')
+
   -- LSP Support
   use {
     'neovim/nvim-lspconfig',
